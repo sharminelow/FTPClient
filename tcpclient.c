458d651829b7b18d6134cb5f156d958b6718dd08
sharmine <slxmlow@gmail.com>
2015-09-30 02:09:50 +0800

    able to select port

--- tcpclient.c
+++ tcpclient.c
@@ -13,7 +13,6 @@
 #include <signal.h>
 
 #define SIZE 1024
-#define FTP_PORT 21
 
 #define USER "USER "
 #define PASS "PASS "
@@ -30,6 +29,7 @@ void oneRecv(int sock, char recv_data[SIZE]);
 void totalRecv(int sock, int isWrite, FILE *fp);
 void totalSend(int sock, FILE *fp);
 
+void getHostAndPort();
 void printMenu();
 void signIn(int sock);
 void listDirectory(int sock);
@@ -43,7 +43,8 @@ int enterEpsvMode(int sock);
 int connectFtpServer(int port);
 
 // global variables
-char hostname[SIZE] = "";
+char _hostname[SIZE] = "";
+int _port = 0;
 
 int main()
 
@@ -72,7 +73,7 @@ void makeDecision(int *sock, char *userChoice)
     //remove newline character and replace with \0
     userChoice[strlen(userChoice) - 1] = '\0';
     int userChoiceInt = atoi(userChoice);
-    
+
     // Validify the user input
     if (userChoiceInt < 1 || userChoiceInt > 8 || strlen(userChoice) > 1)
     {
@@ -82,15 +83,16 @@ void makeDecision(int *sock, char *userChoice)
     }
     else if((*sock == 0 && userChoiceInt != 1) && userChoiceInt != 7)
     {
-        printf("Please connect to a FTP server(choice 1) first\n");
+        printf("\nPlease connect to a FTP server(choice 1) first\n");
         return;
     } 
-    
+
     // Demultiplex decisions
     switch(userChoiceInt)
     {
         case 1:
-            *sock = connectFtpServer(FTP_PORT);
+            getHostAndPort();
+            *sock = connectFtpServer(_port);
             signIn(*sock);
             break;
         case 2:
@@ -113,6 +115,312 @@ void makeDecision(int *sock, char *userChoice)
             break;
     }
 
+}
+
+void getHostAndPort()
+{
+    char buffer[SIZE] = "";
+
+    //if(_hostname != NULL && _hostname[0] == '\0'){
+    printf("\nMini-FTP client# Which FTP server do you want to connect to? : ");
+    fflush(stdout);
+    fgets(_hostname, SIZE, stdin);
+    //remove newline character and replace with \0
+    _hostname[strlen(_hostname) - 1] = '\0';
+    //}
+    
+    printf("Mini-FTP client# Which port? : ");
+    fflush(stdout);
+    fgets(buffer, SIZE, stdin);
+    //remove newline character and replace with \0
+    //_hostname[strlen(_hostname) - 1] = '\0';
+    _port = atoi(buffer);
+}
+
+// This function connect to a FTP server
+// Returns the socket descriptor
+int connectFtpServer(int port)
+{
+    int sock;  
+    char recv_data[SIZE];
+    struct hostent *host;
+    struct sockaddr_in server_addr;  
+
+    host = gethostbyname(_hostname);
+
+    //create a socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket");
+        exit(1);
+    }
+
+    char *host_addr = (*host).h_addr;
+    struct in_addr address = *(struct in_addr *)host_addr;
+
+    server_addr.sin_family = AF_INET;     
+    server_addr.sin_port = htons(port);   
+    server_addr.sin_addr = address;
+
+    // reset bits to 0
+    memset(&(server_addr.sin_zero), 0, 8); 
+
+    if (connect(sock, (struct sockaddr *)&server_addr,
+                sizeof(struct sockaddr)) == -1) 
+    {
+        perror("Connect");
+        exit(1);
+    }
+
+    printf("\nMini-FTP client# Currently connected to host-%s, port-%d\n",
+            inet_ntoa(server_addr.sin_addr),ntohs(server_addr.sin_port));
+    fflush(stdout);
+
+    return sock;
+}
+
+void signIn(int sock)
+{
+    char username[512] = USER;
+    char password[512] = PASS;
+    char buffer[512] = "";
+    char recv_data[SIZE] = "";
+
+    oneRecv(sock, recv_data); // 220 msg - connection success
+    printf("Mini-FTP client# Signing in to FTP server\n");
+    fflush(stdout);
+
+    memset(buffer, 0, 512);
+    printf("Mini-FTP client# Enter your username: ");
+    fgets(buffer, 512, stdin);
+    strcat(username, buffer);
+    oneSend(sock, username);
+    oneRecv(sock, recv_data); // 331 msg - specify password
+
+    memset(buffer, 0, 512);
+    printf("Mini-FTP client# Enter your password: ");
+    fgets(buffer, 512, stdin);
+    strcat(password, buffer);
+    oneSend(sock, password);
+    oneRecv(sock, recv_data); // 230 msg - Login successful
+}
+
+void printWorkingDirectory(int sock)
+{
+    char recv_data[SIZE];
+    oneSend(sock, PWD);
+    oneRecv(sock, recv_data);
+}
+
+void changeWorkingDirectory(int sock)
+{
+    char recv_data[SIZE]; 
+    char userInput[SIZE] = CWD;
+    char buffer[512];
+
+    printf("\nMini-FTP client# Which directory do you want to change to? : ");
+    fflush(stdout);
+    fgets(buffer, SIZE, stdin);
+
+    strcat(userInput, buffer);
+
+    oneSend(sock, userInput); // CWD <path>
+    oneRecv(sock, recv_data); // 250 Directory changed
+
+}
+
+void listDirectory(int sock)
+{
+    char recv_data[SIZE];
+    int port_issued = enterEpsvMode(sock); 
+
+    oneSend(sock, LIST);
+
+    int processId = fork();
+
+    // if childprocess, connect to new port
+    if (processId == 0)
+    {
+        // close the main connection socket
+        close(sock);
+        int connectionSocket = connectFtpServer(port_issued);    
+
+        //receive and print output
+        totalRecv(connectionSocket, 0, NULL);
+
+        // close socket and terminate process
+        close(connectionSocket);
+        exit(0);
+    }
+
+    else 
+    {
+        oneRecv(sock, recv_data); // 150 ascii message
+        wait();
+        totalRecv(sock, 0, NULL); // transfer complete message
+
+    }
+}
+
+void uploadFile(int sock)
+{
+    char recv_data[SIZE] = "";
+    char userInput[SIZE] = STORE;
+    char filename[512] = "";
+
+    printf("\nMini-FTP client# Which file do you want to upload? : ");
+    fflush(stdout);
+    fgets(filename, SIZE, stdin);
+
+    strcat(userInput, filename); // put testfile.txt
+
+    // replace filename last char with \0
+    filename[strlen(filename) - 1] = '\0';
+
+    // check if file exists locally
+    if (access (filename, F_OK ) == -1 ) 
+    {
+        printf("\nMini-FTP client# This file do not exist locally\n");
+        fflush(stdout); 
+        return;
+    } 
+
+    // enter epsv
+    int portIssued = enterEpsvMode(sock);
+    oneSend(sock, userInput);
+
+    int processId = fork();
+
+    if(processId == 0)
+    {
+        // close the main connection socket
+        close(sock);
+
+        int connectionSocket = connectFtpServer(portIssued);
+
+        // open file to read from
+        FILE *fp = fopen(filename, "r");
+        totalSend(sock, fp);
+        fclose(fp);
+
+        // close connection socket
+        close(connectionSocket);
+
+        exit(0);
+    }
+
+    else
+    {
+        oneRecv(sock, recv_data); // 150 msg - okay to send data
+        if (strstr(recv_data, "150"))
+        {
+            wait();
+            oneRecv(sock, recv_data); // 226 msg - transfer complete
+        }
+        else {
+            // kill children process
+            kill(processId, SIGKILL);
+        } 
+    }
+
+    printf("\nMini-FTP client# End of file upload\n");
+    fflush(stdout);
+}
+
+void retrieveFile(int sock)
+{
+    char recv_data[SIZE]; 
+    char userInput[SIZE] = RETRIEVE;
+    char filename[512];
+
+    printf("Mini-FTP client# Which file do you want to download? : ");
+    fflush(stdout);
+    fgets(filename, SIZE, stdin);
+
+    strcat(userInput, filename);
+
+    // replace filename last char with \0
+    filename[strlen(filename) - 1] = '\0';
+
+    int portIssued = enterEpsvMode(sock);
+    oneSend(sock, userInput);
+
+    int processId = fork();
+
+    if(processId == 0)
+    {
+        // close the main connection socket
+        close(sock);
+
+        int connectionSocket = connectFtpServer(portIssued);
+
+        // open file to write
+        FILE *fp = fopen(filename, "w");
+        totalRecv(sock, 1, fp); // to write full data to file
+        fclose(fp);
+
+        // close connection socket
+        close(connectionSocket);
+
+        exit(0);
+    }
+    else
+    {
+        oneRecv(sock, recv_data); // 150 msg - opening ascii mode
+        if (strstr(recv_data, "150"))
+        {
+            wait();
+            oneRecv(sock, recv_data); // 226 msg - transfer complete
+        }
+        else {
+            // kill children process
+            kill(processId, SIGKILL);
+        } 
+    }
+
+}
+
+int enterEpsvMode(int sock)
+{
+    char ftpType[6] = EPSV;
+    char recv_data[SIZE] = "";
+
+    oneSend(sock, ftpType);
+    oneRecv(sock, recv_data); // 229 msg - entering epsv
+
+    //Retrieve port number
+    char *ret = strstr(recv_data, "|");
+    char port[6];
+    memcpy(port, &ret[3], 5);
+    port[6] = '\0';
+    int port_int = atoi(port);
+
+    return port_int;
+}
+
+void printMenu()
+{
+    printf("\n*******************************************\n");
+    printf("**************Mini-FTP client**************\n");
+    printf("*******************************************\n");
+    printf("   1. Connect to FTP server\n");
+    printf("   2. Print Working Directory\n");
+    printf("   3. Change Working Directory\n");
+    printf("   4. List all files\n");
+    printf("   5. Upload File\n");
+    printf("   6. Download File\n");
+    printf("   7. Quit\n");
+    printf("   Enter Option (1-7): ");
+    fflush(stdout);
+
+}
+
+void ftpQuit(int sock)
+{
+    char recv_data[SIZE] = "";
+
+    oneSend(sock, QUIT);
+    oneRecv(sock, recv_data); //must receive goodbye message
+    exit(0);
 }
 
 // this does 1 send call
@@ -162,7 +470,7 @@ void totalRecv(int sock, int isWrite, FILE *fp)
         numBytes = recv(sock, buffer, SIZE-1, 0);
         //printf("RECEIVED NUMBER OF BYTES: %d \n", numBytes);
         //fflush(stdout);
-        
+
         if (numBytes <= 0) // if no more data
         {
             break;
@@ -209,14 +517,14 @@ void totalSend(int sock, FILE *fp)
 
         // read and send
         numBytesRead = fread(buffer, 1, sizeof(buffer), fp);
-        
+
         if (numBytesRead <= 0) // if no more data to be read
         {
-            printf("\nMini-FTP client: The total number of bytes sent: %d\n", totalBytesSend);
+            printf("\nMini-FTP client# The total number of bytes sent: %d\n", totalBytesSend);
             fflush(stdout);
             break;
         }
-        
+
         // Uncomment for debugging
         //printf("READ NUM of BYTES: %d \n", numBytesRead);
         //fflush(stdout);
@@ -233,289 +541,4 @@ void totalSend(int sock, FILE *fp)
         memset(buffer, 0, SIZE);
     }
 
-}
-
-// This function connect to a FTP server, given a port
-// Returns the socket descriptor
-int connectFtpServer(int port)
-{
-    int sock;  
-    char recv_data[SIZE];
-    struct hostent *host;
-    struct sockaddr_in server_addr;  
-
-    if(hostname != NULL && hostname[0] == '\0'){
-        printf("Mini-FTP client: Which FTP server do you want to connect to?: ");
-        fflush(stdout);
-        fgets(hostname, SIZE, stdin);
-        //remove newline character and replace with \0
-        hostname[strlen(hostname) - 1] = '\0';
-    }
-
-    host = gethostbyname(hostname);
-
-    //create a socket
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        perror("Socket");
-        exit(1);
-    }
-
-    char *host_addr = (*host).h_addr;
-    struct in_addr address = *(struct in_addr *)host_addr;
-    
-    server_addr.sin_family = AF_INET;     
-    server_addr.sin_port = htons(port);   
-    server_addr.sin_addr = address;
-    //server_addr.sin_addr = *((struct in_addr *)host->h_addr);
-    memset(&(server_addr.sin_zero), 0, 8); 
-
-    if (connect(sock, (struct sockaddr *)&server_addr,
-                sizeof(struct sockaddr)) == -1) 
-    {
-        perror("Connect");
-        exit(1);
-    }
-
-    printf("\nMini-FTP client: Currently connected to host-%s, port-%d\n",
-            inet_ntoa(server_addr.sin_addr),ntohs(server_addr.sin_port));
-    fflush(stdout);
-
-    return sock;
-}
-
-void signIn(int sock)
-{
-    char username[512] = USER;
-    char password[512] = PASS;
-    char buffer[512] = "";
-    char recv_data[SIZE] = "";
-    
-    oneRecv(sock, recv_data); // 220 msg - connection success
-    printf("Mini-FTP client: Signing in to FTP server\n");
-    fflush(stdout);
-
-    memset(buffer, 0, 512);
-    printf("Mini-FTP client: Enter your username: ");
-    fgets(buffer, 512, stdin);
-    strcat(username, buffer);
-    oneSend(sock, username);
-    oneRecv(sock, recv_data); // 331 msg - specify password
-
-    memset(buffer, 0, 512);
-    printf("Mini-FTP client: Enter your password: ");
-    fgets(buffer, 512, stdin);
-    strcat(password, buffer);
-    oneSend(sock, password);
-    oneRecv(sock, recv_data); // 230 msg - Login successful
-}
-
-void printWorkingDirectory(int sock)
-{
-    char recv_data[SIZE];
-    oneSend(sock, PWD);
-    oneRecv(sock, recv_data);
-}
-
-void changeWorkingDirectory(int sock)
-{
-    //TODO could overflow, what if more tha 512
-    char recv_data[SIZE]; 
-    char userInput[SIZE] = CWD;
-    char buffer[512];
-
-    printf("Mini-FTP client: Which directory do you want to change to? : ");
-    fflush(stdout);
-    fgets(buffer, SIZE, stdin);
-    
-    strcat(userInput, buffer);
-    
-    oneSend(sock, userInput); // CWD <path>
-    oneRecv(sock, recv_data); // 250 Directory changed
-    
-}
-
-void listDirectory(int sock)
-{
-    char recv_data[SIZE];
-    int port_issued = enterEpsvMode(sock); 
-
-    oneSend(sock, LIST);
-
-    int processId = fork();
-
-    // if childprocess, connect to new port
-    if (processId == 0)
-    {
-        // close the main connection socket
-        close(sock);
-        int connectionSocket = connectFtpServer(port_issued);    
-        
-        //receive and print output
-        totalRecv(connectionSocket, 0, NULL);
-
-        // close socket and terminate process
-        close(connectionSocket);
-        exit(0);
-    }
-
-    else 
-    {
-        oneRecv(sock, recv_data); // 150 ascii message
-        wait();
-        totalRecv(sock, 0, NULL); // transfer complete message
-
-    }
-}
-
-void uploadFile(int sock)
-{
-    char recv_data[SIZE] = "";
-    char userInput[SIZE] = STORE;
-    char filename[512] = "";
-
-    printf("\nMini-FTP client: Which file do you want to upload? : ");
-    fflush(stdout);
-    fgets(filename, SIZE, stdin);
-
-    strcat(userInput, filename); // put testfile.txt
-
-    // replace filename last char with \0
-    filename[strlen(filename) - 1] = '\0';
-
-    // check if file exists locally TODO
-    if (access (filename, F_OK ) == -1 ) 
-    {
-        printf("\nMini-FTP client: This file do not exist locally\n");
-        fflush(stdout); 
-        return;
-    } 
-    
-    // enter epsv
-    int portIssued = enterEpsvMode(sock);
-    oneSend(sock, userInput);
-    
-    int processId = fork();
-
-    if(processId == 0)
-    {
-        // close the main connection socket
-        close(sock);
-        connectFtpServer(portIssued);
-
-        // open file to read from
-        FILE *fp = fopen(filename, "r");
-        totalSend(sock, fp);
-        fclose(fp);
-        exit(0);
-    }
-
-    else
-    {
-        oneRecv(sock, recv_data); // 150 msg - okay to send data
-        if (strstr(recv_data, "150"))
-        {
-            wait();
-            oneRecv(sock, recv_data); // 226 msg - transfer complete
-        }
-        else {
-            // kill children process
-            kill(processId, SIGKILL);
-        } 
-    }
-
-    printf("\nMini-FTP client: End of file upload\n");
-    fflush(stdout);
-}
-
-void retrieveFile(int sock)
-{
-    //TODO could overflow, what if more than 512
-    char recv_data[SIZE]; 
-    char userInput[SIZE] = RETRIEVE;
-    char filename[512];
-
-    printf("Mini-FTP client: Which file do you want to download? : ");
-    fflush(stdout);
-    fgets(filename, SIZE, stdin);
-    
-    strcat(userInput, filename);
-    
-    // replace filename last char with \0
-    filename[strlen(filename) - 1] = '\0';
-    
-    int portIssued = enterEpsvMode(sock);
-    oneSend(sock, userInput);
-    
-    int processId = fork();
-
-    if(processId == 0)
-    {
-        // close the main connection socket
-        close(sock);
-        connectFtpServer(portIssued);
-
-        // open file to write
-        FILE *fp = fopen(filename, "w");
-        totalRecv(sock, 1, fp); // to write full data to file
-        fclose(fp);
-        exit(0);
-    }
-    else
-    {
-        oneRecv(sock, recv_data); // 150 msg - opening ascii mode
-        if (strstr(recv_data, "150"))
-        {
-            wait();
-            oneRecv(sock, recv_data); // 226 msg - transfer complete
-        }
-        else {
-            // kill children process
-            kill(processId, SIGKILL);
-        } 
-    }
-    
-}
-
-int enterEpsvMode(int sock)
-{
-    char ftpType[6] = EPSV;
-    char recv_data[SIZE] = "";
-
-    oneSend(sock, ftpType);
-    oneRecv(sock, recv_data); // 229 msg - entering epsv
-
-    //Retrieve port number
-    char *ret = strstr(recv_data, "|");
-    char port[6];
-    memcpy(port, &ret[3], 5);
-    port[6] = '\0';
-    int port_int = atoi(port);
-
-    return port_int;
-}
-
-void printMenu()
-{
-    printf("\n*******************************************\n");
-    printf("**************Mini-FTP client**************\n");
-    printf("*******************************************\n");
-    printf("   1. Connect to FTP server\n");
-    printf("   2. Print Working Directory\n");
-    printf("   3. Change Working Directory\n");
-    printf("   4. List all files\n");
-    printf("   5. Upload File\n");
-    printf("   6. Download File\n");
-    printf("   7. Quit\n");
-    printf("   Enter Option (1-7): ");
-    fflush(stdout);
-
-}
-
-void ftpQuit(int sock)
-{
-    char recv_data[SIZE] = "";
-
-    oneSend(sock, QUIT);
-    oneRecv(sock, recv_data); //must receive goodbye message
-    exit(0);
 }
